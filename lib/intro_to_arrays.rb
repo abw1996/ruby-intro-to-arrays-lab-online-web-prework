9c8176bab071d9af172200ef4525519ac063d8e0
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-04-30 23:30:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  thang = Array.new 
+  return thang
+end
+
+def array_with_two_elements 
+  thang = ['two','elements']
+  return thang
+end
+
+def my_first_element(argument)
+  return argument[1]
+end
+
+def last_element(argument)
+  return argument[-1]
+end
+
+def third_element(argument)
+  return argument[2]
+end
+
