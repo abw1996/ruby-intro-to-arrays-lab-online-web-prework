ff495419e029ba8090b38f800c34dabd857f3e8a
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-04-30 23:59:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -24,3 +24,10 @@ def first_element_with_array_methods(argument)
   argument.first
 end
   
+def last_element_with_array_methods(argument)
+  argument.last 
+end
+
+def length_of_array(argument)
+  argument.length 
+end
